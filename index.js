bf1c9a21a1b6d436887ee069b89eb32a08464220
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-04-07 09:58:25 +0600

    connect with database and collection created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,27 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// connect with database
+const dbConnect = async () => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
+dbConnect();
+
+// database collection;
+const userCollection = client.db("address_manager").collection("contacts");
 
 // root api
 app.get("/", (req, res) => {
