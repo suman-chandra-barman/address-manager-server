8ec8e9dcd9b7b08b2ba19010243123e4b01cd645
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-04-07 14:22:02 +0600

    contact create and get api done

--- index.js
+++ index.js
@@ -29,7 +29,42 @@ const dbConnect = async () => {
 dbConnect();
 
 // database collection;
-const userCollection = client.db("address_manager").collection("contacts");
+const contactCollection = client.db("address_manager").collection("contacts");
+
+app.post("/contacts", async (req, res) => {
+  try {
+    const { name, phone, email, image } = req.body;
+
+    // Check if mobile number already exists in contacts collection
+    const existingContact = await contactCollection.findOne({ phone });
+    if (existingContact) {
+      return res
+        .status(409)
+        .json({ message: "Mobile number already exists in contacts." });
+    }
+
+    // Insert new contact into contacts collection
+    const result = await contactCollection.insertOne({
+      name,
+      phone,
+      email,
+      image,
+    });
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.get("/contacts", async (req, res) => {
+  try {
+    const query = {};
+    const contacts = await contactCollection
+      .find(query)
+      .sort({ name: 1 })
+      .toArray();
+    res.send(contacts);
+  } catch (error) {}
+});
 
 // root api
 app.get("/", (req, res) => {
